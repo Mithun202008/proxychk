4dfa21c48076d8fec7da6b08f53eee4eff5bac9f
Mithun <mithunanbazhagan@gmail.com>
2026-08-24 23:25:18 +0530

    bump: release v1.0.4

--- installers/uninstall.ps1
+++ installers/uninstall.ps1
@@ -1,7 +1,7 @@
 # ================================================================
 #  PROXC — Windows Uninstaller (PowerShell)
 #  Author  : Mithun A
-#  Version : v1.0.2
+#  Version : v1.0.4
 # ================================================================
 
 $ErrorActionPreference = "Continue"
